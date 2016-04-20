e1b568c831bb4af6966a0dd47dc383a264bd613d
unknown <english@GCC-PC6008.student.gcc.ie>
2016-04-20 13:15:03 +0100

    Milestone 3.

--- app/src/main/java/com/example/english/assignment3/MainActivity.java
+++ app/src/main/java/com/example/english/assignment3/MainActivity.java
@@ -1,29 +1,84 @@
 package com.example.english.assignment3;
 
 import android.app.Activity;
+import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
+
+    boolean traced;
+    LocationManager locationManager;
+    TextView tx;
+    String locationProvider;
+    LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button start = (Button)findViewById(R.id.start);
+        final ArrayList<Location> list = new ArrayList<>();
+        final int i = 0;
+        traced = false;
+        locationProvider = LocationManager.NETWORK_PROVIDER;
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                tx.setText("lat, long: " + location.getLatitude() + ", " + location.getLongitude());
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+        };
+        final Button start = (Button) findViewById(R.id.start);
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(start.getText().toString() == "STOP"){
+                if (start.getText().toString() == "STOP") {
                     start.setText("START");
-                }else{
+                } else {
                     start.setText("STOP");
+                }
+                traced = !traced;
+                Log.d("actived = ", "" + traced);
+                if (traced) {
+                    tx = (TextView) findViewById(R.id.text);
+                    Location l = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                    if (l != null) {
+                        if(list.size() < 101) {
+                            list.add(l);
+                        }else{
+                            list.remove(0);
+                            list.add(l);
+                        }
+                        double longitude = l.getLongitude();
+                        double latitude = l.getLatitude();
+                    }
+                    locationManager.requestLocationUpdates(locationProvider, 5, 0, locationListener);
                 }
             }
         });
@@ -49,24 +104,5 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    private final class MyLocation implements LocationListener {
-        @Override
-        public void onLocationChanged(Location location) {
-            
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-        }
     }
 }
